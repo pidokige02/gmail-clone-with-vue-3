0b1fa13f97afd482b5ec2af78a01302190f9414b
pidokige02 <pidokige0204@gmail.com>
2022-03-14 18:53:49 +0900

    commit for lesson9

--- src/components/ModalView.vue
+++ src/components/ModalView.vue
@@ -8,22 +8,27 @@
 </template>
 
 <script>
-  import { onBeforeUnmount } from 'vue'
+  // import { onBeforeUnmount } from 'vue'
+  import useKeydown from '../composables/use-keydown';
 
   export default {
     setup(props, {emit}) {
-      let onKeydown = (event) => {
-        console.log(event.key)
+      // let onKeydown = (event) => {
+      //   console.log(event.key)
 
-        if(event.key == 'Escape') {
-          emit('closeModal')
-        }
-      }
+      //   if(event.key == 'Escape') {
+      //     emit('closeModal')
+      //   }
+      // }
 
-      window.addEventListener('keydown', onKeydown)
-      onBeforeUnmount(()=>{
-        window.removeEventListener('keydown', onKeydown)
-      })
+      // window.addEventListener('keydown', onKeydown)
+      // onBeforeUnmount(()=>{
+      //   window.removeEventListener('keydown', onKeydown)
+      // })
+
+      useKeydown([
+        {key: 'Escape', fn: () => { emit('closeModal') }},
+      ])
 
       return {
         emit
