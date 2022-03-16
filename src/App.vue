223abc5745c4390a0bcd3126e9513c57c26e262a
pidokige02 <pidokige0204@gmail.com>
2022-03-16 23:02:55 +0900

    commit for the final2

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <h1>VMail Inbox</h1>
 
-
   <Suspense>
     <template #default>
       <MailTable />
@@ -11,13 +10,20 @@
     </template>
   </Suspense>
 </template>
-  
+
 <script>
 import MailTable from '@/components/MailTable.vue';
+import useEmailSelection from '@/composables/use-email-selection';
+
 export default {
   name: 'App',
   components: {
     MailTable
+  },
+  setup(){
+    return {
+      emailSelection: useEmailSelection()
+    }
   }
 };
 </script>
